ae00e8b5fb3213e63ff2eb898b19c54d8d7d9f24
hanseul <gkstmf616@gmail.com>
2024-01-25 17:45:35 +0900

    add all pages

--- crawling-quote.js
+++ crawling-quote.js
@@ -1,34 +1,54 @@
-import * as cheerio from 'cheerio';
-import axios from 'axios';
+import * as cheerio from "cheerio";
+import axios from "axios";
 //const axios = require('axios'); //import와 같은 의미
 
-const url = `https://quotes.toscrape.com/`;
-// https://quotes.toscrape.com/page/(숫자) 로 페이지 넘어감
+const baseUrl = `https://quotes.toscrape.com/`;
+let currentPage = 1; //처음엔 1페이지
+let isNext = true; //다음 페이지가 있는지 없는지
 
-axios.get(url, ).then(response=>{
-    const html = response.data;
-    const $ = cheerio.load(html);
+const quotes = [];
 
-    const quotes=[];
-    $('.quote').map((index,element)=>{
-        const quote = $(element).find('.text').text().trim();
-        const author = $(element).find('.author').text().trim();
-        const authorUrl = $(element).find('a').attr('href');
-        const tags=[];
-        $(element).find('.tag').map((i,tagElement)=>{
+//재귀함수로 페이지 반복하기
+const fetchQuote = (url) => {
+  axios.get(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $(".quote").map((index, element) => {
+        const quote = $(element).find(".text").text().trim();
+        const author = $(element).find(".author").text().trim();
+        const authorUrl = $(element).find("a").attr("href");
+        const tags = [];
+        $(element)
+          .find(".tag")
+          .map((i, tagElement) => {
             tags.push($(tagElement).text().trim());
-        });
-        
+          });
+
         quotes.push({
-            quote: quote,
-            author:author,
-            authorUrl: `https://quotes.toscrape.com${authorUrl}`,
-            tags: tags
+          quote: quote,
+          author: author,
+          authorUrl: `https://quotes.toscrape.com${authorUrl}`,
+          tags: tags,
         });
+      });
+
+      //다음 페이지 확인
+      const nextPageLink = $('.next a').attr('href'); // /page/숫자
+      if(nextPageLink){
+        currentPage++;
+        const nextUrl=baseUrl+nextPageLink;
+        
+        // https://quotes.toscrape.com/page/(숫자) 로 페이지 넘어감
+        fetchQuote(nextUrl);
+      }else{
+        isNext=false; //다음 페이지 없으면 isNext=false 시키고 멈춤
+        console.log(quotes);
+      }
+    })
+    .catch((err) => {
+      console.error("ERROR: ",err);
     });
-
-    console.log(quotes);
-
-}).catch(err=>{
-    console.error(err);
-})
+};
+fetchQuote(baseUrl);
